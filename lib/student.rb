deb8734e6ab6e13e8b76498f3c4689357ead8cab
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-24 00:11:19 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -92,7 +92,7 @@ end
       self.new_from_db(row)
     end
   end
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
     SELECT *
     FROM students
@@ -100,9 +100,32 @@ end
     LIMIT ?
   SQL
   
-  DB[:conn].execute(sql, x).each do |row|
-    self.new_from_db(sql, x)
+  DB[:conn].execute(sql, num).map do |row|
+    self.new_from_db(row)
   end
+  end 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students 
+    WHERE grade = 10
+    LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students 
+    WHERE grade = ?
+    SQL
+ 
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end 
   
 end
